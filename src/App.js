80f96f275de8b37c8e8a459d3f7325e96f00e413
dianaJ1 <108881394+dianaJ1@users.noreply.github.com>
2022-10-11 14:21:28 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import Dictionary from "./Dictionary";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <header className="App-header"></header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>Coded by Diana Jethwa</small>
+        </footer>
+      </div>
+    </div>
+  );
+}
